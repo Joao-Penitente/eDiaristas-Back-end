3b6fb25a668683bd9a39a93e9edf00050729f734
João Vitor Penitente <joaov.penitente@gmail.com>
2022-05-18 21:26:44 -0300

    seeder

--- database/factories/DiaristaFactory.php
+++ database/factories/DiaristaFactory.php
@@ -17,7 +17,19 @@ class DiaristaFactory extends Factory
     public function definition()
     {
         return [
-            //
+        'nome_completo' => $this->faker->name(),
+        'cpf' => '11111000000',
+        'email' => $this->faker->unique()->safeEmail(),
+        'telefone' => '99999999999',
+        'logradouro' => '$this->faker->streetName()',
+        'numero' => 3,
+        'bairro' => '$this->faker->secondaryAddress()',
+        'complemento' => '$this->faker->secondaryAddress()',
+        'cidade' => 'colatina',
+        'cep' => '29933570',
+        'estado' => 'es',
+        'codigo_ibge' => 3204906,
+        'foto_usuario'  => '',
         ];
     }
 }
